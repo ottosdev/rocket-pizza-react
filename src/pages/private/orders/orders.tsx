c4c346473f791da7b9ab0624f0d9ec9d10de332e
Otto Souza <ottosouza.dev@gmail.com>
2024-01-16 20:53:52 -0300

    Pagination

--- src/pages/private/orders/orders.tsx
+++ src/pages/private/orders/orders.tsx
@@ -1,5 +1,13 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Pagination } from '@/components/pagination.tsx'
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table.tsx'
 import { OrderTableFilters } from '@/pages/private/orders/order-table-filters.tsx'
 import { OrdersTableRow } from '@/pages/private/orders/orders-table-row.tsx'
 
@@ -9,12 +17,30 @@ export function Orders() {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderTableFilters />
 
-        <div className="rounded-md border">
-          <OrdersTableRow />
+        <div className="space-y-2.5">
+          <OrderTableFilters />
+
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificados</TableHead>
+                  <TableHead className="w-[180px]">Realizado</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total de pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <OrdersTableRow />
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
